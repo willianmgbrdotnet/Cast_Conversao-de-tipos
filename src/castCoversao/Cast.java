6925e5578c25b8d3e3b94d86f7c30c8480e6faea
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-08-07 18:22:52 -0300

    Declaração Literal

--- src/castCoversao/Cast.java
+++ src/castCoversao/Cast.java
@@ -13,6 +13,14 @@ public class Cast {
         //No entanto, nota-se que que apenas a parte inteira do numero com valor flutuante foi atribuida.
         //Print: 2750
 
+        int valor2 = 2000_000_000;
+        //é necessário colocar o "L" depois do numero para declarar que é um numero Literal.
+        long numeroGrande = 5000_000_000_000_000_000L;
+        short numeroPequeno = 2105;
+        byte umaCentena = 127;
 
+        //Numeros decimais do tipo float têm precisão menor. O java recomenda double.
+        //Precisa declarar de forma literal "f" caso queira insistir.
+        float pontoFlutuante = 3.14f;
     }
 }
